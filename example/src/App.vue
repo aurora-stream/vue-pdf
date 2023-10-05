d7e4f63f756d56dec4f316668da55f722a8c81b3
dd <62934849+luobingme@users.noreply.github.com>
2023-10-05 22:48:40 +0800

    d

--- example/src/App.vue
+++ example/src/App.vue
@@ -89,4 +89,11 @@ function setMode(type: fitType) {
   width: 100%;
   margin: 20px 0;
 }
+.content {
+  border: 1px solid #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
+
